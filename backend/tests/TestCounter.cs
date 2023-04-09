8fde2a1b39136c01ca29020f6f22aa53c40308f0
Takakazu Saito <stakakazu85@gmail.com>
2023-04-09 15:03:47 -0400

    Updates to html

--- backend/tests/TestCounter.cs
+++ backend/tests/TestCounter.cs
@@ -17,13 +17,13 @@ namespace tests
         private readonly ILogger logger = TestFactory.CreateLogger();
 
         [Fact]
-        public async void Http_trigger_should_return_known_string()
+        public async Task Http_trigger_should_return_known_string()
         {
             var counter = new Company.Function.Counter();
             counter.Id = "1";
             counter.Count = 2;
             var request = TestFactory.CreateHttpRequest();
-            var response = (HttpResponseMessage) Company.Function.GetResumeCounter.Run(request, counter, out counter, logger);
+            var response = (HttpResponseMessage) Company.Function.GetVisitorCounter.Run(request, counter, out counter, logger);
             Assert.Equal(3, counter.Count);
         }
 

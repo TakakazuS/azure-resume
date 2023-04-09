55e4ba6716dd4d5dd70f9c93b4ef17cf4b7c5483
Takakazu Saito <stakakazu85@gmail.com>
2023-04-09 14:31:11 -0400

    Updates to html

--- backend/api/GetVisitorCounter.cs
+++ backend/api/GetVisitorCounter.cs
@@ -4,34 +4,33 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 
-namespace Company.Function;
-
-public class GetVisitorCounter
+namespace Company.Function
 {
-    private readonly ILogger _logger;
-
-    public GetVisitorCounter(ILoggerFactory loggerFactory)
+    public static class GetVisitorCounter
     {
-        _logger = loggerFactory.CreateLogger<GetVisitorCounter>();
-    }
+        [Function("GetVisitorCounter")]
+        public static MyOutputType Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+            [CosmosDBInput(databaseName: "AzureResume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] Counter counter,
+            [CosmosDBInput(databaseName: "AzureResume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] out Counter UpdatedCounter,
+            ILogger log)
+        { 
+            // Here is where the counter gets updated.
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            
+            UpdatedCounter = counter;
+            UpdatedCounter.Count += 1;
 
-    [Function("GetVisitorCounter")]
-    public MyOutputType Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
-        [CosmosDBInput(databaseName: "AzureResume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] Counter counter,
-        [CosmosDBInput(databaseName: "AzureResume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] out Counter UpdatedCounter)
-    { 
-        UpdatedCounter = counter;
-        UpdatedCounter.Count += 1;
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "application/json; charset=utf-8");
+            string jsonString = JsonSerializer.Serialize(counter);
+            response.WriteString(jsonString);
         
-        var response = req.CreateResponse(HttpStatusCode.OK);
-        response.Headers.Add("Content-Type", "application/json; charset=utf-8");
-        string jsonString = JsonSerializer.Serialize(counter);
-        response.WriteString(jsonString);
-        
-        return new MyOutputType()
-        {
-            HttpResponse = response
-        };
+            return new MyOutputType()
+            {
+                HttpResponse = response
+            };
+        }
     }
+    
 }

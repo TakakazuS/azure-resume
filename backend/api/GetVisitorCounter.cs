0619f9e0118a7217c3ce60a0a0670b57e10182dc
Takakazu Saito <stakakazu85@gmail.com>
2023-04-11 22:32:06 -0400

    Updates to function

--- backend/api/GetVisitorCounter.cs
+++ backend/api/GetVisitorCounter.cs
@@ -8,16 +8,18 @@ namespace Company.Function;
 
 public class GetVisitorCounter
 {
-    private readonly ILogger logger;
+    private readonly ILogger _logger;
 
     public GetVisitorCounter(ILoggerFactory loggerFactory)
     {
-        logger = loggerFactory.CreateLogger<GetVisitorCounter>();
+        _logger = loggerFactory.CreateLogger<GetVisitorCounter>();
     }
 
     [Function("GetVisitorCounter")]
     public MyOutputType Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
-    [CosmosDBInput(databaseName: "AzureResume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] Counter counter)
+    [CosmosDBInput(databaseName: "AzureResume", collectionName: 
+    "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1",
+            PartitionKey = "1")] Counter counter)
     {
 
         var response = req.CreateResponse(HttpStatusCode.OK);
